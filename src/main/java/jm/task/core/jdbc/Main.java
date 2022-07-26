739a4f0c85493ebfe381ebaa04acd73872ffc0fa
Евгений <Khodov1992@mail.ru>
2022-07-26 22:49:04 +0500

    commit

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,29 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        UserDao userDao = new UserDaoJDBCImpl();
+        userDao.createUsersTable();
+        userDao.saveUser("Vasily", "Pupkin", (byte) 20);
+        userDao.saveUser("Geralt", "OfRivia", (byte) 40);
+        userDao.saveUser("Yennefer", "OfVengerberg", (byte) 31);
+        userDao.saveUser("Jaskier", "JustJaskier", (byte) 25);
+        System.out.println(userDao.getAllUsers());
+        userDao.removeUserById(1);
+        System.out.println(userDao.getAllUsers());
+        userDao.getAllUsers();
+        System.out.println(userDao.getAllUsers());
+        userDao.cleanUsersTable();
+        System.out.println(userDao.getAllUsers());
+        userDao.dropUsersTable();
         // реализуйте алгоритм здесь
     }
 }
